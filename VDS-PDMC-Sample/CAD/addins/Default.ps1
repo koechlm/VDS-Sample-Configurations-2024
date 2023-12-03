0b3e0b493828400e211707f21c2e01e38607b2cd
Markus Koechl <markus.koechl@autodesk.com>
2023-12-03 10:51:34 +0100

    added default CAD, Vault to PDMC-Sample

---
+++ VDS-PDMC-Sample/CAD/addins/Default.ps1
@@ -0,0 +1,182 @@
+﻿
+function InitializeWindow
+{
+	#begin rules applying commonly
+    $dsWindow.Title = SetWindowTitle		
+    InitializeCategory
+    InitializeNumSchm
+    InitializeBreadCrumb
+    InitializeFileNameValidation
+	#end rules applying commonly
+	$mWindowName = $dsWindow.Name
+	switch($mWindowName)
+	{
+		"InventorWindow"
+		{
+			#rules applying for Inventor
+		}
+		"AutoCADWindow"
+		{
+			#rules applying for AutoCAD
+		}
+	}
+	$global:expandBreadCrumb = $true	
+}
+
+function AddinLoaded
+{
+	#Executed when DataStandard is loaded in Inventor/AutoCAD
+}
+
+function AddinUnloaded
+{
+	#Executed when DataStandard is unloaded in Inventor/AutoCAD
+}
+
+function InitializeCategory()
+{
+    if ($Prop["_CreateMode"].Value)
+    {
+		if (-not $Prop["_SaveCopyAsMode"].Value)
+		{
+            $Prop["_Category"].Value = $UIString["CAT1"]
+        }
+    }
+}
+
+function InitializeNumSchm()
+{
+	#Adopted from a DocumentService call, which always pulls FILE class numbering schemes
+	$global:numSchems = @($vault.NumberingService.GetNumberingSchemes('FILE', 'Activated')) 
+    if ($Prop["_CreateMode"].Value)
+    {
+		if (-not $Prop["_SaveCopyAsMode"].Value)
+		{
+			$Prop["_Category"].add_PropertyChanged({
+				if ($_.PropertyName -eq "Value")
+				{
+					$numSchm = $numSchems | where {$_.Name -eq $Prop["_Category"].Value}
+                    if($numSchm)
+					{
+                        $Prop["_NumSchm"].Value = $numSchm.Name
+                    }
+				}	
+			})
+        }
+		else
+        {
+            $Prop["_NumSchm"].Value = "None"
+        }
+    }
+}
+
+function GetVaultRootFolder()
+{
+    $mappedRootPath = $Prop["_VaultVirtualPath"].Value + $Prop["_WorkspacePath"].Value
+    $mappedRootPath = $mappedRootPath -replace "\\", "/" -replace "//", "/"
+    if ($mappedRootPath -eq '')
+    {
+        $mappedRootPath = '$'
+    }
+    return $vault.DocumentService.GetFolderByPath($mappedRootPath)
+}
+
+function SetWindowTitle
+{
+	$mWindowName = $dsWindow.Name
+    switch($mWindowName)
+ 	{
+  		"InventorFrameWindow"
+  		{
+   			$windowTitle = $UIString["LBL54"]
+  		}
+  		"InventorDesignAcceleratorWindow"
+  		{
+   			$windowTitle = $UIString["LBL50"]
+  		}
+  		"InventorPipingWindow"
+  		{
+   			$windowTitle = $UIString["LBL39"]
+  		}
+  		"InventorHarnessWindow"
+  		{
+   			$windowTitle = $UIString["LBL44"]
+  		}
+  		default #applies to InventorWindow and AutoCADWindow
+  		{
+   			if ($Prop["_CreateMode"].Value)
+   			{
+    			if ($Prop["_CopyMode"].Value)
+    			{
+     				$windowTitle = "$($UIString["LBL60"]) - $($Prop["_OriginalFileName"].Value)"
+    			}
+    			elseif ($Prop["_SaveCopyAsMode"].Value)
+    			{
+     				$windowTitle = "$($UIString["LBL72"]) - $($Prop["_OriginalFileName"].Value)"
+    			}else
+    			{
+     				$windowTitle = "$($UIString["LBL24"]) - $($Prop["_OriginalFileName"].Value)"
+    			}
+   			}
+   			else
+   			{
+    			$windowTitle = "$($UIString["LBL25"]) - $($Prop["_FileName"].Value)"
+   			} 
+  		}
+ 	}
+  	return $windowTitle
+}
+
+function GetNumSchms
+{
+	$specialFiles = @(".DWG",".IDW",".IPN")
+    if ($specialFiles -contains $Prop["_FileExt"].Value -and !$Prop["_GenerateFileNumber4SpecialFiles"].Value)
+    {
+        return $null
+    }
+	if (-Not $Prop["_EditMode"].Value)
+    {
+		if ($numSchems.Count -gt 1)
+		{
+			$numSchems = $numSchems | Sort-Object -Property IsDflt -Descending
+		}
+        if ($Prop["_SaveCopyAsMode"].Value)
+        {
+            $noneNumSchm = New-Object 'Autodesk.Connectivity.WebServices.NumSchm'
+            $noneNumSchm.Name = $UIString["LBL77"]
+            return $numSchems += $noneNumSchm
+        }    
+        return $numSchems
+    }
+}
+
+function GetCategories
+{
+	return $Prop["_Category"].ListValues
+}
+
+function OnPostCloseDialog
+{
+	$mWindowName = $dsWindow.Name
+	switch($mWindowName)
+	{
+		"InventorWindow"
+		{
+			#rules applying for Inventor
+		}
+		"AutoCADWindow"
+		{
+			#rules applying for AutoCAD
+			if ($Prop["_CreateMode"]) {
+				#the default ACM Titleblocks expect the file name and drawing number as attribute values; adjust property(=attribute) names for custom titleblock definitions
+				$dc = $dsWindow.DataContext
+				$Prop["GEN-TITLE-DWG"].Value = $dc.PathAndFileNameHandler.FileName
+				$Prop["GEN-TITLE-NR"].Value = $dc.PathAndFileNameHandler.FileNameNoExtension
+			}
+		}
+		default
+		{
+			#rules applying commonly
+		}
+	}
+}
---
+++ VDS-PDMC-Sample/Vault/addinVault/AssociatedFiles.ps1
@@ -0,0 +1,112 @@
+﻿
+Add-Type @'
+public class myAssocFile
+{
+	public string link;
+	public string key;
+	public string componenttype;
+	public string filename;
+	public string version;
+	public string title;
+	public string revision;
+	public string description;
+}
+'@
+
+function GetAssociatedFiles($itemids, $iconLocation)
+{
+	$dsDiag.Trace(">> Starting GetAssociatedFiles($itemids)")
+	$primary = [Autodesk.Connectivity.Webservices.ItemFileLnkTypOpt]::Primary
+	$secondary = [Autodesk.Connectivity.Webservices.ItemFileLnkTypOpt]::Secondary
+	$tertiary = [Autodesk.Connectivity.Webservices.ItemFileLnkTypOpt]::Tertiary
+	$standard = [Autodesk.Connectivity.Webservices.ItemFileLnkTypOpt]::StandardComponent
+	$primarysub = [Autodesk.Connectivity.Webservices.ItemFileLnkTypOpt]::PrimarySub
+	$secondarysub = [Autodesk.Connectivity.Webservices.ItemFileLnkTypOpt]::SecondarySub
+	$assocFiles = $vault.ItemService.GetItemFileAssociationsByItemIds($itemids, $primary -bor $secondary -bor $tertiary -bor $standard -bor $primarysub -bor $secondarysub)
+	$PropDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FILE")
+	$descriptiondef = $PropDefs | Where-Object { $_.SysName -eq "description"}
+	$titledef = $PropDefs | Where-Object { $_.SysName -eq "title"}
+	$propDefIds = $descriptiondef.Id, $titledef.Id
+	$assocFileIds = @()
+	$assocFiles | ForEach-Object{
+		$assocFileIds += $_.CldFileId
+	}
+	$myAssocFiles = @()
+	$files = @()
+	if ($assocFileIds.Count -gt 0 )
+	{
+		$files = $vault.DocumentService.GetFilesByIds($assocFileIds)
+	}
+	[System.Collections.Generic.Dictionary[Int,Object]]$fileDic = @{}
+	$files | ForEach-Object{
+		$fileDic[$_.Id] = $_
+	}
+	$props = $vault.PropertyService.GetProperties("FILE",$assocFileIds,$propDefIds)
+	$assocFiles | ForEach-Object { 
+		$file = $fileDic[$_.CldFileId]
+		$fileext = $([System.IO.Path]::GetExtension($file.Name)).Substring(1)
+		$myFile = New-Object myAssocFile
+		$myFile.filename = $file.Name
+		$myFile.version = $file.VerNum
+		$myFile.revision = $file.FileRev.Label
+		$titleprop = $props | Where-Object { ($file.Id -eq $_.EntityId) -and ($_.PropDefId -eq $titledef.Id)}
+		$myFile.title = $titleprop.Val
+		$descriptionprop = $props | Where-Object { ($file.Id -eq $_.EntityId) -and ($_.PropDefId -eq $descriptiondef.Id)}
+		$myFile.description = $descriptionprop.Val
+		$path = GetPath $iconLocation $fileext
+		$exists = Test-Path $path
+		if (-not $exists)
+		{
+			$path = GetPath $iconLocation "unknown"
+		}
+		$myFile.Componenttype = $path
+		$keypath = GetPath $iconLocation $_.Typ
+		$myFile.key = $keypath
+		$linkpath = GetPath $iconLocation 'linkedfile'
+		$myFile.link = $linkpath
+		$myAssocFiles += $myFile
+	}
+	
+	$assocAttachments = $vault.ItemService.GetAttachmentsByItemIds($itemids)
+	$attfileIds =@()
+	$assocAttachments | ForEach-Object {
+		$_.AttmtArray | ForEach-Object {
+			$attfileIds += $_.FileId
+		}
+	}
+	if ($attfileIds.Count -gt 0 )
+	{
+		$attFiles = $vault.DocumentService.GetFilesByIds($attfileIds)
+		$attProps = $vault.PropertyService.GetProperties("FILE",$attfileIds,$propDefIds)
+		$attFiles | ForEach-Object {
+			$fileext = $([System.IO.Path]::GetExtension($_.Name)).Substring(1)
+			$myAttFile = New-Object myAssocFile
+			$myAttFile.filename = $_.Name
+			$myAttFile.version = $_.VerNum
+			$myAttFile.revision = $_.FileRev.Label
+			$fileId = $_.Id
+			$titleprop = $attProps | Where-Object { ($fileId -eq $_.EntityId) -and ($_.PropDefId -eq $titledef.Id)}
+			$myAttFile.title = $titleprop.Val
+			$descriptionprop = $attProps | Where-Object { ($fileId -eq $_.EntityId) -and ($_.PropDefId -eq $descriptiondef.Id)}
+			$myAttFile.description = $descriptionprop.Val
+
+			$path = GetPath $iconLocation $fileext
+			$exists = Test-Path $path
+			if (-not $exists)
+			{
+				$path = GetPath $iconLocation "unknown"
+			}
+			$myAttFile.Componenttype = $path
+			$keypath = GetPath $iconLocation 'attachment'
+			$myAttFile.link = $keypath
+			$myAssocFiles += $myAttFile
+		}
+	}
+	return $myAssocFiles
+}
+
+function GetPath($iconLocation,$name)
+{
+	$iconpath = [string]::Format("Icons\{0}.png", $name)
+	return [System.IO.Path]::Combine($iconLocation,$iconpath)
+}
